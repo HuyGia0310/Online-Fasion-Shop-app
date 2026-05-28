5915d4779f7c795567387dc82e74aa1af2071139
HuyGia0310 <huyphamgia03102004@gmail.com>
2026-05-28 14:45:01 +0700

    chỉnh sửa phần chọn Size

--- app/src/main/java/ntu/huy/shopapp/Adapter/SizeAdapter.java
+++ app/src/main/java/ntu/huy/shopapp/Adapter/SizeAdapter.java
@@ -14,55 +14,55 @@ import ntu.huy.shopapp.R;
 import ntu.huy.shopapp.databinding.ViewholderSizeBinding;
 
 public class SizeAdapter extends RecyclerView.Adapter<SizeAdapter.Viewholder> {
-    ArrayList<String> items;
-    Context context;
-    int selectedPosition = -1;
-    int lastSelectedPosition = -1;
+        ArrayList<String> items;
+        Context context;
+        int selectedPosition = -1;
+        int lastSelectedPosition = -1;
 
-    public SizeAdapter(ArrayList<String> items) {
-        this.items = items;
-    }
+        public SizeAdapter(ArrayList<String> items) {
+            this.items = items;
+        }
 
-    @NonNull
-    @Override
-    public SizeAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        context =parent.getContext();
-        ViewholderSizeBinding binding=ViewholderSizeBinding.inflate(LayoutInflater.from(context),parent,false);
+        @NonNull
+        @Override
+        public SizeAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            context =parent.getContext();
+            ViewholderSizeBinding binding=ViewholderSizeBinding.inflate(LayoutInflater.from(context),parent,false);
 
-        return new Viewholder(binding);
-    }
+            return new Viewholder(binding);
+        }
 
-    @Override
-    public void onBindViewHolder(@NonNull SizeAdapter.Viewholder holder, int position) {
-        holder.binding.sizeTxt.setText(items.get(position));
-        holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                lastSelectedPosition=selectedPosition;
-                selectedPosition=holder.getAdapterPosition();
-                notifyItemChanged(lastSelectedPosition);
-                notifyItemChanged(selectedPosition);
+        @Override
+        public void onBindViewHolder(@NonNull SizeAdapter.Viewholder holder, int position) {
+            holder.binding.sizeTxt.setText(items.get(position));
+            holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    lastSelectedPosition=selectedPosition;
+                    selectedPosition=holder.getAdapterPosition();
+                    notifyItemChanged(lastSelectedPosition);
+                    notifyItemChanged(selectedPosition);
+                }
+            });
+            if(selectedPosition==holder.getAdapterPosition()){
+                holder.binding.sizeLayout.setBackgroundResource(R.drawable.size_selected);
+                holder.binding.sizeTxt.setTextColor(context.getResources().getColor(R.color.purple));
+            }else {
+                holder.binding.sizeLayout.setBackgroundResource(R.drawable.size_unselected);
+                holder.binding.sizeTxt.setTextColor(context.getResources().getColor(R.color.black));
             }
-        });
-        if(selectedPosition==holder.getAdapterPosition()){
-            holder.binding.sizeLayout.setBackgroundResource(R.drawable.size_selected);
-            holder.binding.sizeTxt.setTextColor(context.getResources().getColor(R.color.purple));
-        }else {
-            holder.binding.sizeLayout.setBackgroundResource(R.drawable.size_unselected);
-            holder.binding.sizeTxt.setTextColor(context.getResources().getColor(R.color.black));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        public class Viewholder extends RecyclerView.ViewHolder {
+            ViewholderSizeBinding binding;
+            public Viewholder(ViewholderSizeBinding binding) {
+                super(binding.getRoot());
+                this.binding=binding;
+            }
         }
     }
-
-    @Override
-    public int getItemCount() {
-        return items.size();
-    }
-
-    public class Viewholder extends RecyclerView.ViewHolder {
-        ViewholderSizeBinding binding;
-        public Viewholder(ViewholderSizeBinding binding) {
-            super(binding.getRoot());
-            this.binding=binding;
-        }
-    }
-}
